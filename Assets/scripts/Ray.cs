b281829b8bbdc352fd9afa848f801bfc16ac3645
Simon Raes <simon.raes@inthepocket.mobi>
2016-10-25 12:59:16 +0200

    Only allow jumping when grounded or on a wall.

--- Assets/scripts/Ray.cs
+++ Assets/scripts/Ray.cs
@@ -3,7 +3,7 @@
 public class Ray : MonoBehaviour
 {
     public LayerMask collisionMask;
-    BoxCollider2D boxCollider;
+
     RaycastOrigins raycastOrigins;
     Vector3 movement;
     const float skinWidth = .015f;
@@ -14,45 +14,132 @@ public class Ray : MonoBehaviour
     public float gravity = -20;
     public float terminalVelocity = -10;
 
-	public bool grounded;
+    private BoxCollider2D boxCollider;
+    private Renderer rend;
 
+    private bool grounded;
+    private bool bumpedHead;
+    private bool leftWallSlide, rightWallSlide;
 
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        rend = GetComponent<Renderer>();
     }
 
     void Update()
     {
-		if(grounded)
-		{
-			movement.y = 0;
-		}
+        if (grounded || bumpedHead)
+        {
+            movement.y = 0;
+        }
 
-		grounded = false;
+        // ResetCollisions();
 
+        RegisterInputs();
+
+        // CheckForWallSlideLeft();
+        // CheckForWallSlideRight();
+
+        AttemptMove(movement * Time.deltaTime);
+
+        SetPlayerDebugColor();
+    }
+
+    private void ResetCollisions()
+    {
+        leftWallSlide = false;
+        rightWallSlide = false;
+        grounded = false;
+        bumpedHead = false;
+    }
+
+    private void RegisterInputs()
+    {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         movement.x = input.x * moveSpeed;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && (grounded || leftWallSlide || rightWallSlide))
         {
             movement.y = jumpSpeed;
         }
 
         movement.y += gravity * Time.deltaTime;
+    }
 
-        AttemptMove(movement * Time.deltaTime);
+    private void CheckForWallSlideLeft()
+    {
+        float rayLength = .1f;
+
+        Vector2 origin = raycastOrigins.bottomLeft;
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.left, rayLength, collisionMask);
+
+        if (hit && !grounded && hit.distance < .1f)
+        {
+            leftWallSlide = true;
+        }
+		else
+		{
+			leftWallSlide = false;
+		}
+    }
+
+    private void CheckForWallSlideRight()
+    {
+        float rayLength = .1f;
+
+        Vector2 origin = raycastOrigins.bottomRight;
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.right, rayLength, collisionMask);
+
+        if (hit && !grounded && hit.distance < .1f)
+        {
+            rightWallSlide = true;
+        }
+		else{
+			rightWallSlide = false;
+		}
+    }
+
+    void SetPlayerDebugColor()
+    {
+        if (leftWallSlide)
+        {
+            rend.material.color = Color.blue;
+        }
+        else if (rightWallSlide)
+        {
+            rend.material.color = Color.green;
+        }
+        else
+        {
+            if (grounded)
+            {
+                rend.material.color = Color.red;
+            }
+            else
+            {
+                rend.material.color = Color.cyan;
+            }
+        }
+
     }
 
     void AttemptMove(Vector3 movement)
     {
         UpdateRaycastOrigins();
 
-		// CapAtTerminalVelocity(ref movement);
-        CheckHorizontalCollisions(ref movement);
+        // CapAtTerminalVelocity(ref movement);
         CheckVerticalCollisions(ref movement);
 
+	
+
+        CheckHorizontalCollisions(ref movement);
+
+	CheckForWallSlideLeft();
+        CheckForWallSlideRight();
+		
         transform.Translate(movement);
     }
 
@@ -65,28 +152,6 @@ public class Ray : MonoBehaviour
         raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
         raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
         raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-    }
-
-    private void CheckHorizontalCollisions(ref Vector3 movement)
-    {
-        float direction = Mathf.Sign(movement.x);
-        float rayLength = Mathf.Abs(movement.x) + skinWidth;
-
-        Vector2 origin = direction < 0 ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.right * direction, rayLength, collisionMask);
-
-        if (!hit)
-        {
-            // No hit at the bottom, check the top corner
-            origin = direction < 0 ? raycastOrigins.topLeft : raycastOrigins.topRight;
-            hit = Physics2D.Raycast(origin, Vector2.right * direction, rayLength, collisionMask);
-        }
-
-        if (hit)
-        {
-            print("horizontal hit");
-            movement.x = (hit.distance - skinWidth) * direction;
-        }
     }
 
     private void CheckVerticalCollisions(ref Vector3 movement)
@@ -106,19 +171,73 @@ public class Ray : MonoBehaviour
 
         if (hit)
         {
-            print("vertical hit");
             movement.y = (hit.distance - skinWidth) * direction;
 
-			grounded = true;
+            if (direction < 0)
+            {
+                grounded = true;
+            }
+            else
+            {
+                bumpedHead = true;
+            }
         }
+		else{
+			grounded = false;
+			bumpedHead = false;
+		}
     }
+
+    private void CheckHorizontalCollisions(ref Vector3 movement)
+    {
+        float direction = Mathf.Sign(movement.x);
+        float rayLength = Mathf.Abs(movement.x) + skinWidth;
+
+        Vector2 origin = direction < 0 ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.right * direction, rayLength, collisionMask);
+
+        if (!hit)
+        {
+            // No hit at the bottom, check the top corner
+            origin = direction < 0 ? raycastOrigins.topLeft : raycastOrigins.topRight;
+            hit = Physics2D.Raycast(origin, Vector2.right * direction, rayLength, collisionMask);
+        }
+
+        if (hit)
+        {
+            movement.x = (hit.distance - skinWidth) * direction;
+
+            // Can't wallslide if we're on the floor
+            if (!grounded)
+            {
+                if (direction < 0)
+                {
+                    leftWallSlide = true;
+                }
+                else if (direction > 0)
+                {
+                    rightWallSlide = true;
+                }
+            }
+			else{
+				leftWallSlide = false;
+				rightWallSlide = false;
+			}
+        }
+		else{
+			leftWallSlide = false;
+			rightWallSlide = false;
+		}
+    }
+
+
 
     private void CapAtTerminalVelocity(ref Vector3 movement)
     {
-			if(movement.y < terminalVelocity)
-			{
-				movement.y = terminalVelocity;
-			}
+        if (movement.y < terminalVelocity)
+        {
+            movement.y = terminalVelocity;
+        }
     }
 
     // private RaycastHit2D CheckVerticalHit(Vector2 origin)
