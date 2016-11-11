2ca2b658e1cf2b2e6cbb7a1b62521febb85901b5
Simon Raes <simon.raes@inthepocket.mobi>
2016-11-11 22:53:25 +0100

    Add lasers(scene).

---
+++ Assets/scripts/Enemies/Laser.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour
+{
+
+    private LineRenderer lineRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetVertexCount(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, transform.right);
+
+        if (ray)
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, ray.point);
+
+			Player player = ray.collider.gameObject.GetComponent<Player>();
+
+            if (player != null)
+            {
+				player.die();
+            }
+
+            // if(hit player)
+            // {
+            // kill him
+            // }
+        }
+    }
+}
---
+++ Assets/scripts/Mover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed;
+    public Transform start;
+    public Transform end;
+
+	private Vector3 target;
+
+    // Use this for initialization
+    void Start()
+    {
+		transform.position = start.position;
+		target = end.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+		if(Vector3.Distance(transform.position, target) <= .1f)
+		{
+			target = target == start.position ? end.position : start.position;
+		}
+    }
+}
